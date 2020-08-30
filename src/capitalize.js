a16bbdf23f3c8fbc13962f1fae2d35d77ebd47d2
Unknown <unknown@example.com>
2020-08-30 01:16:51 -0500

    webpack

---
+++ src/capitalize.js
@@ -0,0 +1,5 @@
+function Capitalize(text) {
+    return text.substring(0,1).toUpperCase() + text.substring(1, text.length);
+}
+
+export default Capitalize;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const _ = require('lodash');
+
+console.log(_.upperCase("hola mundo"));
+
+console.log(_.capitalize("jeenson"));
