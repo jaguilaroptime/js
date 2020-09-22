a879e68a831c381865a8e670d37aa8e53a9456d6
Jaguilar <jeensonaguilar@hotmail.com>
2020-09-21 21:55:12 -0500

    Usando watcher

--- real-world-vue/src/components/fetch.vue
+++ real-world-vue/src/components/fetch.vue
@@ -1,10 +1,14 @@
 <template>
 <div>
+    <h2>Listado de usuarios</h2>
     <ul>
         <li v-for="usuario in usuarios" :key="usuario.id">
             {{ usuario.name }}
         </li>
     </ul>
+    <h2>Uso del wather</h2>
+    <input type="text" v-model="password">
+    <p>{{ mensaje }}</p>
 </div>
 </template>
 
@@ -14,6 +18,13 @@ export default {
     data: function () {
         return {
             usuarios: [],
+            mensaje: 'Introduce un password',
+            password: null,
+            novalidos: [
+                "admin",
+                "12345",
+                "password"
+            ],
         }
     },
     created() {
@@ -21,6 +32,21 @@ export default {
         fetch(url)
             .then(respuesta => respuesta.json())
                 .then(respuesta => this.usuarios = respuesta);
+    },
+    watch: {
+        password(nuevo) {
+            if (this.novalidos.indexOf(nuevo) > -1) {
+                this.mensaje = "Contaseña no válida";
+                return false;
+            }
+
+            if (nuevo.length < 5) {
+                this.mensaje = "La contraseña debe tener al menos 5 caractertes";
+                return false;
+            }
+
+            this.mensaje = "Contaseña correcta";
+        }
     },
 }
 </script>
