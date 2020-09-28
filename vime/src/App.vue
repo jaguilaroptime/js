051ac65b7284da41947c6a2bb296c1241e8e15d0
Jaguilar <jeensonaguilar@hotmail.com>
2020-09-27 23:36:17 -0500

    Usando Vime - Reproductor de videos

---
+++ vime/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Player />
+  </div>
+</template>
+
+<script>
+import Player from './components/Player';
+
+// Default theme.
+import '@vime/core/themes/default.css';
+
+export default {
+  name: 'App',
+  components: {
+    Player
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vime/src/components/Player.vue
@@ -0,0 +1,43 @@
+<template>
+  <VimePlayer>
+    <!-- Provider component is placed here. -->
+    <VimeVideo crossOrigin="" poster="https://media.vimejs.com/poster.png">
+      <!-- These are passed directly to the underlying HTML5 `<video>` element. -->
+      <!-- Why `data-src`? Lazy loading, you can always use `src` if you prefer.  -->
+      <source 
+        data-src="https://media.vimejs.com/720p.mp4" 
+        type="video/mp4" 
+      />
+      <track 
+        default 
+        kind="subtitles" 
+        src="https://media.vimejs.com/subs/english.vtt" 
+        srclang="en" 
+        label="English" 
+      />
+    </VimeVideo>
+
+    <VimeDefaultUi noControls>
+      <!-- We setup the default controls and pass in any options. -->
+      <VimeDefaultControls
+        hideOnMouseLeave
+        :activeDuration="2000"
+      />
+    </VimeDefaultUi>
+    
+  </VimePlayer>
+</template>
+
+<script>
+  import { VimePlayer,  VimeVideo, VimeDefaultUi , VimeDefaultControls} from '@vime/vue';
+
+  export default {
+    name: 'Player',
+    components: {
+      VimePlayer,
+      VimeVideo,
+      VimeDefaultUi,
+      VimeDefaultControls
+    },
+  };
+</script>
